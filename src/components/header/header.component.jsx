68a2c1af1be770d67d42984c3aada5fccd588307
oscar <oscaroceguera@gmail.com>
2019-09-25 00:58:55 -0600

    integrate firebase into app, created sign in page with sign in component and sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-import './header.styles.scss'
+import { auth } from '../../firebase/firebase.utils'
+
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
-const Header = () => (
+import './header.styles.scss'
+
+const Header = ({ currentUser }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -15,6 +18,24 @@ const Header = () => (
       </Link>
       <Link className='option' to='/contact'>
         CONTACT
+      </Link>
+      <Link className='option' to='/contact'>
+        {currentUser ? (
+          <div
+            className='option'
+            onClick={() => auth.signOut()}
+          >
+            SIGN OUT
+          </div>
+        )
+        : (
+          <Link
+            className='option'
+            to='/signin'
+          >
+            SIGN IN
+          </Link>
+        )}
       </Link>
     </div>
   </div>
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const config = {
+  apiKey: "AIzaSyAxN0R9gSS-7nAP0UlDO0u5Hre6pIX11fs",
+  authDomain: "crwn-db-bd546.firebaseapp.com",
+  databaseURL: "https://crwn-db-bd546.firebaseio.com",
+  projectId: "crwn-db-bd546",
+  storageBucket: "",
+  messagingSenderId: "2680070782",
+  appId: "1:2680070782:web:435c652163d5b47886ae2f"
+}
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+
+const provider = new firebase.auth.GoogleAuthProvider()
+provider.setCustomParameters({ prompt: 'select_account' })
+export const signInWithGoogle = () => auth.signInWithPopup(provider)
+
+export default firebase
