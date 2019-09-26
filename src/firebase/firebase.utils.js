dca1941a57439aedcc4b7b1ebfb550d89be3645d
oscar <oscaroceguera@gmail.com>
2019-09-26 14:14:36 -0600

    Implemented firebase utils, included a bility to store auth user into our firestore data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,34 @@ const config = {
   appId: "1:2680070782:web:435c652163d5b47886ae2f"
 }
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) {
+    return
+  }
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get()
+  
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth
+    const createAt = new Date()
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log("TCL: createUserProfileDocument -> error", error)
+    }
+  }
+
+  return userRef
+}
+
 firebase.initializeApp(config)
 
 export const auth = firebase.auth()
