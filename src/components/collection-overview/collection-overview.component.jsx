8a0ed14c64e36cf276f4354d4a3d4eea94fbb2db
oscar <oscaroceguera@gmail.com>
2020-01-01 22:54:47 -0700

    Section 11: Master Project: Advanced Routing

---
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollections } from "../../redux/shop/shop.selector";
+
+import CollectionPreview from "../preview-collection/collection-preview.component";
+
+import "./collection-overview.styles.scss";
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="collection-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStatetoProps = createStructuredSelector({
+  collections: selectCollections
+});
+
+export default connect(mapStatetoProps)(CollectionsOverview);
---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import { selectCollection } from "../../redux/shop/shop.selector";
+
+import "./collection.scss";
+
+const CollectionPage = ({ collection }) => {
+  console.log("TCL: CollectionPage -> collection", collection);
+  return (
+    <div className="collection">
+      <h2>collection PAGE</h2>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
